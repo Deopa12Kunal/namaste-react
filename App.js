bc9d2ab7fcaca103b967bb27b27b53986dc4e827
kunal <deopakunal@gmail.com>
2024-02-04 13:10:17 +0530

    episode2

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+ 
+console.log(parent);
+ const root= ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parent);
+
+ 
+
+  
